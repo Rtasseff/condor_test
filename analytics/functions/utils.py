a7b922200477c722e40a3806eb3d9a163b319698
rtasseff <rtasseff@gmail.com>
2024-12-12 18:34:39 +0100

    added methods for converting some data: list of asset time courses to a single pandas dataframe and a data frame to a set of numpy arrays

--- analytics/functions/utils.py
+++ analytics/functions/utils.py
@@ -33,4 +33,59 @@ def ns2days(ns):
     
     return days
 
+def asset_list2df(assets):
+    """Take a list of Asset objects, assets, and create a price DataFrame.  
+    A 2D pandas df for the data with rows as dates and cols as the price values
+    """
+    symH = 'Symbol'
+    dateH = 'Date'
+    asset = assets[0]
+    priceH = asset.prices.name
+    sym = asset.sym
+    prices =  asset.prices.values
+    dates = asset.prices.times
+    n = len(prices)
+    if n != len(dates):
+        raise Exception('Asset '+sym+' has a different number of times and values')
+    tmp = np.stack(([sym]*n,dates,prices),axis=1)
+    df = pd.DataFrame(data=tmp,columns=[symH,dateH,prcieH])
+
+    for i in range(1,len(assets),1):
+        asset = assets[i]
+        if priceH != asset.prices.name:
+            raise Exception('Cannot join asset values with different time course names')
+        sym = asset.sym
+        prices =  asset.prices.values
+        dates = asset.prices.times
+        n = len(prices)
+        if n != len(dates):
+            raise Exception('Asset '+sym+' has a different number of times and values')
+
+        tmp = np.stack(([sym]*n,dates,prices),axis=1)
+        df = df.append(tmp)
+
+    # rearrange table as dates vs symbols
+    data = data.pivot_table(index=dateH, columns=symH, values=priceH)
+
+
+    return df
+
+
+
+
+
+
+
+def df2np(df):
+    """Convert a dataframe into a numpy data matrix, 
+    a numpy array for index and column headers.
+    in the condor workflow we have been using simple numpy arrays 
+    and not much on panads after the data is orginized .
+    To facilitate this it is easier for us to just pull out the parts.
+    We typically expect the index to be the dates and the collumns to 
+    be the asset symbols.
+    """
+    return df.to_numpy(), df.index.to_numpy(), df.columns.to_numpy()
+
+
 
