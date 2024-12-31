12a8b83c7f720542002eee72389875008011fcb7
rtasseff <rtasseff@gmail.com>
2024-12-31 15:42:53 +0100

    port updates

--- analytics/classes/CondorCoreObs.py
+++ analytics/classes/CondorCoreObs.py
@@ -10,6 +10,7 @@ analyticsDir = '/Users/rtasseff/projects/condor_test/analytics'
 from functions import genStats as gs
 from functions import genFin as gf
 from functions import utils
+from functions import portOpt as po
 
 from data_mining import load
 
@@ -88,7 +89,7 @@ class PriceLoader:
             self.set_target_asset_symbols(syms) 
 
         # get the data
-        prices, dates, syms = self.get_assets_np(syms=syms)
+        prices, dates, syms = self.get_assets_np(syms=self.syms)
 
 
         # start making the assets
@@ -120,10 +121,15 @@ class PriceLoader:
 
 
 class TimeCourse:
-    def __init__(self, times, values, name='Time Course'):
+    def __init__(self, times, values, name='Time Course', sampInt=1):
         # assuming time as a numpy array of DateTime objects
         # values is a numpy array of corrisponding values
-        # name is arbitrary 
+        # name is arbitrary
+        # You can also set the sampling interval
+        # in the future we may want to create a 'Sampler' object to pass or 
+        # use seperatly in between getting values here and the calculation
+        # because there are at least two things intervale (frequency) 
+        # and period) that we will need, possibly more (like flaging)
         if len(times) != len(values):
             print(len(values))
             print(len(times))
@@ -132,6 +138,20 @@ class TimeCourse:
         self.values = values
         self.name = name
         self.lastUpdated = datetime.datetime.now() # need to double check format
+
+        self.sampInt = sampInt
+
+    def _sample(self,array):
+        # return the values, but properly sampled
+        n = len(array)
+        inds = np.arange(0,n,self.sampInt)
+        return array[inds]
+
+    def sample_values(self):
+        # return the values, but properly sampled
+        return self._sample(self.values)
+    def sample_dates(self):
+        return self._sample(self.dates)
 
 
 class Asset:
@@ -155,8 +175,7 @@ class Asset:
         self.prices=prices
 
         if self.priceLoader.syms[0] != sym:
-            self.priceLoader.syms = [sym]
-            raise Warning('Passed PriceLoader has a different symbol definintion than the symbol you passed. PriceLoader, '+  priceLoader.syms[0] +', was overriden with '+sym)
+            raise Exception('Passed PriceLoader has a different symbol definintion than the symbol you passed. PriceLoader symbol, '+  priceLoader.syms[0] +', asset symbol '+sym)
 
 
 
@@ -169,6 +188,11 @@ class Asset:
         self.expectedReturn = None
         self.returnDispersion = None
 
+        # *** Hard coded from a Project on S&P 500
+        # need to find a better way to deal with this
+        # like a 'Sampler' object to pass ***
+        self.sampInt = 20
+
 
 
     def update_prices(self, prices=None):
@@ -180,7 +204,6 @@ class Asset:
             # no price, then load data
             # a bit akward but if everything was defined correctly in its setup,
             # we just need (maybe later we can add some checks to loader):
-            print(priceLoader.syms)
             prices = self.priceLoader.get_assets()[0].prices
         
         # maybe later we can add some checks here on the prices, 
@@ -228,7 +251,7 @@ class Asset:
         if self.get_prices_lastUpdated() is None:
             raise Exception('Prices have never been updated.  You need prices to calculate the returns.')
         self.returns = Returns(self.prices, timeFrame=timeFrame, metric=metric, 
-                method=method)
+                method=method, sampInt=self.sampInt)
         self.expectedReturn = self.returns.calc_expected()
         self.returnDispersion = self.returns.calc_dispersion()
 
@@ -250,7 +273,7 @@ class Asset:
             
 
 class Returns(TimeCourse):
-    def __init__(self, prices, timeFrame='M', metric='Relative', method='Robust'):
+    def __init__(self, prices, timeFrame='M', metric='Relative', method='Robust', sampInt=1):
         # prices is a TimeCourse object
         if timeFrame == 'D':
             timePeriod = 1
@@ -266,16 +289,16 @@ class Returns(TimeCourse):
         values = gf.returns(prices.values, period=timePeriod, metric=metric)
         times = prices.times[timePeriod:]
 
-        super().__init__(times, values, name = timeFrame+'ly '+metric+' Returns')
+        super().__init__(times, values, name = timeFrame+'ly '+metric+' Returns', sampInt=sampInt)
                 
         self.method = method
 
     def calc_expected(self):
-        return gf.returnExp(self.values, method=self.method)
+        return gf.returnExp(self.sample_values(), method=self.method)
 
     def calc_dispersion(self):
         # added a way to return co-dispersion squared if this is a matrix
-        x = self.values
+        x = self.sample_values()
         if len(x.shape)==2:
             y = gf.returnCoDispSq(x,method=self.method) 
         else:
@@ -321,6 +344,9 @@ class Portfolio:
         self.method=None
         self.metric=None
         self.timeFrame=None
+
+        # *** this is built in as default temporarily as in Asset ***
+        self.sampInt = 20
 
 
 
@@ -387,7 +413,7 @@ class Portfolio:
         if self.get_prices_lastUpdated() is None:
             raise Exception('Prices have never been updated.  You need prices to calculate the returns.')
         self.returns = Returns(self.prices, timeFrame=timeFrame, metric=metric, 
-                method=method)
+                method=method, sampInt = self.sampInt)
         self.expectedReturnArray = self.returns.calc_expected()
         self.returnCoDispersionSqMatrix = self.returns.calc_dispersion()
 
@@ -410,7 +436,7 @@ class Portfolio:
 
 
     def update_properties(self, weights=None, timeFrame='M', metric='Relative', 
-            method='Robust', annualize=True):
+            method='Robust', annualize=False):
         # This literlly updates everything starting with prices from data and then returns.
         # One can change the weights if a new set of weigths is passed
         self.update_prices()
@@ -431,11 +457,11 @@ class Portfolio:
                 self.expectedReturnArray, self.returnCoDispersionSqMatrix, annualizeBy=annualizeBy)
 
         # save for later
-        self.annualize = annualize
+        self.annualizeBy = annualizeBy
         
 
 
-    def calc_peroperties(self, w=None, annualize=None):
+    def calc_properties(self, weights=None, annualize=None):
         # this assumes asset expected returns and dispersion matrix exist, 
         # which would have required prices and returns.
         # You can only call this after a set of returns for assets has been calculated.
@@ -448,22 +474,133 @@ class Portfolio:
         # how the returns were calculated (timeFrame in anything that updates returns).
         # if it is not set it uses the past setting when the returns were calculated.
         # If somehow nothing is set (which should not work) then it defulats to False
+        # in this case we are both returning and overwriting
         
         if self.get_returns_lastUpdated() is None:
             raise Exception('Returns have never been updated.  You need returns to calculate the properties.')
-
+        # *** this is messy and I think the logic needs fixin
         if annualize is None:
-            if self.annualizeBy is None:
-                self.annualizeBy = 'None' 
+            # go to previous setting
+            annualizeBy = self.timeFrame
         else:
-            self.annualizeBy = annualizeBy
+            # decide what to do
+            if annualize:
+                # we are annualizing
+                annualizeBy = self.timeFrame
+            else:
+                annualizeBy = 'None'
+
 
         # update the weights if passed
         if weights is not None:
             self.set_weights(weights)
 
-        self.expectedReturn, self.returnDispersion = gf.asset_set_perform(self.weights, 
-                self.expectedReturnArray, self.returnCoDispersionSqMatrix, annualizeBy=annualizeBy)
+        expectedReturn, returnDispersion = gf.asset_set_perform(self.weights, 
+                self.expectedReturnArray, self.returnCoDispersionSqMatrix, 
+                annualizeBy=annualizeBy)
+
+        self.expectedReturn = expectedReturn
+        self.returnDispersion = returnDispersion
+
+        return expectedReturn, returnDispersion
+
+    def get_syms(self):
+        if type(self.assets[0]) is str:
+            # only reasonable choice is this is the symbol list
+            syms = assets
+        else:
+            # assuming it is an asset list
+            syms = []
+            for asset in assets:
+                syms.append(asset.sym)
+
+        return syms
+
+    def calc_sharpe_ratio(self,riskFreeRate=0, weights=None, annualize=None):
+        # we caculate and return the Sharpe Ratio (see genFin.py for info) of this portfolio.
+        # we allow the user to explore different weights by passing them
+        # the user can also turn the annualize off or on if passed
+        # in this case nothing is overwritten 
+
+        if self.get_returns_lastUpdated() is None:
+            raise Exception('No returns set for this portfolio.  You must first update returns.')
+
+        if weights is None:
+            # no wieghts passed, use the existing ones:
+            weights = self.weights
+
+        # this is the second time I have called similar mess logic on annualize trying to 
+        # make the user interface easier at the cost of this mess
+        # must be a better way to do this or just shove it all in one function
+        if annualize is None:
+            # go to previous setting
+            annualizeBy = self.timeFrame
+        else:
+            # decide what to do
+            if annualize:
+                # we are annualizing
+                annualizeBy = self.timeFrame
+            else:
+                annualizeBy = 'None'
+
+
+
+        sr = gf.asset_set_sharpe_ratio(weights, self.expectedReturnArray, 
+                self.returnCoDispersionSqMatrix, riskFreeRate=riskFreeRate, 
+                annualizeBy=annualizeBy)
+
+        return sr
+
+    def optimize_sharpe_ratio(self,riskFreeRate=0, annualize=None):
+        # Optimize this portfolio weigths to mazimize its sharpe ratio
+        # Sets (overrides) the current portfolio weights to maximize the sr
+        # returns the max sr
+        if self.get_returns_lastUpdated() is None:
+            raise Exception('No returns set for this portfolio.  You must first update returns.')
+
+        # this is the third time I have called similar mess logic on annualize trying to 
+        # make the user interface easier at the cost of this mess
+        # must be a better way to do this or just shove it all in one function
+        if annualize is None:
+            # go to previous setting
+            annualizeBy = self.timeFrame
+        else:
+            # decide what to do
+            if annualize:
+                # we are annualizing
+                annualizeBy = self.timeFrame
+            else:
+                annualizeBy = 'None'
+        
+        # run existing optimizer
+        optResults = po.max_sharpe_ratio(self.expectedReturnArray, 
+                self.returnCoDispersionSqMatrix, riskFreeRate=riskFreeRate,annualizeBy=annualizeBy)
+
+        # get the weights
+        wSRMax = optResults['x']
+        
+        # This calculates and sets (overrides) portfolio properties
+        expectedReturn, returnDispersion = self.calc_properties(weights=wSRMax, annualize=annualize)
+
+        # calculate the sharpe ratio
+        sr = self.calc_sharpe_ratio(riskFreeRate=riskFreeRate, annualize=annualize)
+
+        # if all is as expected this should be the negative SR
+        negSR = optResults['fun']
+        # let us confirm since it is a relativly fast calc and right now 
+        # condor has lots of moving parts
+        # this can confirm multiple steps at once for concitancy 
+        # *** maybe remove later once a more stable version is reached ***
+        if np.abs(negSR + sr) > 0.0001 :
+            print(negSR)
+            print(sr)
+            raise Exception('The Sharpe ratio function in the optimizer is not matching the one in the portfolio, could be a code issues somewhere, contact the developer.')
+
+        return sr
+
+
+
+
 
 
 
